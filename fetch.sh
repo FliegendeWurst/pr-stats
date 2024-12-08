bab715162693b102575ae1c0a01febc83c0aa25e
FliegendeWurst <2012gdwu+github@posteo.de>
2024-12-08 11:54:09 +0100

    Update DB

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,8 @@
-owner=Homebrew
-repo=homebrew-cask
+owner=NixOS
+repo=nixpkgs
 
 mkdir data{,_monthly,_monthly_merged}/$owner
+cargo build --release --bin gen_csv --bin gen_csv_monthly --bin gen_csv_monthly_merged
 cargo run --release --bin gen_csv -- $owner $repo > data/$owner/$repo.csv &
 cargo run --release --bin gen_csv_monthly -- $owner $repo > data_monthly/$owner/$repo.csv &
 cargo run --release --bin gen_csv_monthly_merged -- $owner $repo > data_monthly_merged/$owner/$repo.csv &
